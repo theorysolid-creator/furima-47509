92237dc02745e82d55b8aafb8687532974674461
theorysolid-creator <theorysolid@gmail.com>
2026-03-14 09:41:37 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,10 +12,14 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path # 保存できたらトップへ
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+  @item = Item.find(params[:id])
   end
 
   private
