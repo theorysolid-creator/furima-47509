c1de520b7354e3c2cb098e8ac9a435382a8ccfea
theorysolid-creator <theorysolid@gmail.com>
2026-03-18 16:49:56 +0900

    挙動確認修正対応1

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,11 +5,11 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンを入れてください (例 123-4567)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
-    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'は10桁または11桁の半角数値で入力してください' }
+    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid"}
     validates :token # クレジットカード決済用のトークン
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,16 @@ class User < ApplicationRecord
   # 2. パスワードは、半角英数字混合での入力が必須
   # 6文字以上のバリデーションはDeviseの標準機能に含まれています
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で入力してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
   # 3. お名前(全角)は、名字と名前がそれぞれ必須。全角（漢字・ひらがな・カタカナ）での入力が必須
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
     validates :first_name
     validates :last_name
   end
 
   # 4. お名前カナ(全角)は、名字と名前がそれぞれ必須。全角（カタカナ）での入力が必須
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
     validates :first_name_kana
     validates :last_name_kana
   end
